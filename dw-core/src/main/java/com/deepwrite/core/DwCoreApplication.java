aeae40bb6107f73bdeb4e9681720cffb1fb945ad
张紫豪 <1131220792@qq.com>
2025-12-02 19:24:42 +0800

    修复：添加 MyMetaObjectHandler 自动填充时间字段，解决 createdAt 为空报错

--- dw-core/src/main/java/com/deepwrite/core/DwCoreApplication.java
+++ dw-core/src/main/java/com/deepwrite/core/DwCoreApplication.java
@@ -2,12 +2,14 @@ package com.deepwrite.core;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.apache.dubbo.config.spring.context.annotation.EnableDubbo;
 
 @SpringBootApplication
 @EnableDiscoveryClient
 @EnableDubbo
+@ComponentScan("com.deepwrite")
 public class DwCoreApplication {
     public static void main(String[] args) {
         SpringApplication.run(DwCoreApplication.class, args);
